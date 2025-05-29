e20a558a8261cb724b3e3bc4b6793be03add7604
Johan Agus <johanagus.it@gmail.com>
2025-05-29 13:25:39 +0700

    core : setup logrus for loging app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"os"
 
+	"github.com/johanagus/simple-pos/router"
+	"github.com/sirupsen/logrus"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	logger := logrus.New()
+
+	file, err := os.OpenFile("./log/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	router.New(app)
+
+	logger.SetOutput(file)
+	logger.Fatal(app.Listen(":8000"))
 }
--- router/router.go
+++ router/router.go
@@ -1,5 +1,9 @@
 package router
 
-func New() {
+import "github.com/gofiber/fiber/v2"
 
+func New(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
 }
