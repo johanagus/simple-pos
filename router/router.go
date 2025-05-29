3191dfad2dbe69f131e894cd26254285c7520b32
Johan Agus <johanagus.it@gmail.com>
2025-05-29 17:06:06 +0700

    feat : setup router

--- router/router.go
+++ router/router.go
@@ -1,9 +1,36 @@
 package router
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/johanagus/simple-pos/handlers"
+)
 
 func New(app *fiber.App) {
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
+
+	api := app.Group("/api/v1")
+
+	api.Post("/auth", handlers.Auth)
+
+	// api.Get("/users")
+	// api.Get("/user/:id")
+	// api.Post("/user")
+	// api.Put("/user/:id")
+	// api.Delete("/user/id:
+
+	// api.Get("/products")
+	// api.Get("/product/:id")
+	// api.Post("/product")
+	// api.Put("/product/:id")
+	// api.Delete("/product/:id
+
+	// api.Get("/categories")
+	// api.Get("/category/:id")
+	// api.Post("/category")
+	// api.Put("/category/:id")
+	// api.Delete("/category/:id")
+
 }
